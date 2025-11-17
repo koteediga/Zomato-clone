6a27805795dee96149380d5dfa630c584967fb24
koteswararao <koteediga85@gmail.com>
2025-11-17 17:13:03 +0530

    Review is done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { LoginComponent } from './components/LoginComponent/LoginComponent';
 import { Home } from './components/HomeComponent/Home';
-import  ProtectedRoute  from './components/ProtectedComponent/ProtectedRoute';
-import {Switch, Route} from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedComponent/ProtectedRoute';
+import { Switch, Route } from 'react-router-dom';
 import Cart from './components/Cart/Cart';
 import PageNotFound from './components/NotFound/NotFound';
 import RestaurantDetail from './components/RestaurantDetail/RestaurantDetail';
@@ -11,38 +11,27 @@ import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-      <CartProvider>
-    <div className="App">
-     <Switch>
-  <Route path="/login" component={LoginComponent} />
-  <Route  exact
-    path="/" 
-    render={() => (
-      <ProtectedRoute>
-        <Home />
-      </ProtectedRoute>
-    )}
-  />
-  <Route  exact
-    path="/cart" 
-    render={() => (
-      <ProtectedRoute>
-        <Cart />
-      </ProtectedRoute>
-    )}
-  />
-  <Route exact
-    path="/restaurants/:restaurantId" 
-    render={() => (
-      <ProtectedRoute>
-        <RestaurantDetail />
-      </ProtectedRoute>
-    )} 
-  />
-  {/* Fallback route for undefined URLs */}
-  <Route component={PageNotFound} />
-</Switch>
-    </div>
+    <CartProvider>
+      <div className="App">
+        <Switch>
+
+          {/* Public route */}
+          <Route exact path="/login" component={LoginComponent} />
+
+          {/* Protected routes */}
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <ProtectedRoute 
+            exact 
+            path="/restaurants/:restaurantId" 
+            component={RestaurantDetail} 
+          />
+
+          {/* 404 fallback */}
+          <Route component={PageNotFound} />
+
+        </Switch>
+      </div>
     </CartProvider>
   );
 }
